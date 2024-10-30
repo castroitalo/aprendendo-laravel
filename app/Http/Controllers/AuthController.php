b4989fb392b2667c7d7147593dbf0a3482d334e6
username <dev.castro.italo@gmail.com>
2024-10-30 20:40:11 -0300

    login done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use PDOException;
@@ -28,12 +29,13 @@ class AuthController extends Controller
     }
 
     /**
-     * Submit login form
+     * Submit login attempt
      *
-     * @param Request $request Login form request data
-     * @return void
+     * @param Request $request User login request data
+     * @return null|RedirectResponse
+     * @throws BindingResolutionException
      */
-    public function loginSubmit(Request $request): void
+    public function loginSubmit(Request $request): ?RedirectResponse
     {
         // Validate form
         $request->validate(
@@ -65,12 +67,40 @@ class AuthController extends Controller
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        // Get all users from database
-        $userModel = new User();
-        $users     = $userModel->all()
-            ->toArray();
+        // Check if user exists
+        $user = User::where('username', $username)
+            ->where('deleted_at', NULL)
+            ->first();
 
-        dd($users);
+        if (!$user) {
+            return redirect()->back()
+                ->withInput()
+                ->with('loginError', 'Nao existe nenhum registro com esse e-mail.');
+        }
+
+        // Validate password
+        if (!password_verify($password, $user->password)) {
+            return redirect()->back()
+                ->withInput()
+                ->with('loginError', 'Senha ou email incorretos.');
+        }
+
+        // Update user's last login
+        $user->last_login = date('Y-m-d H:i:s');
+
+        $user->save();
+
+        // Update session with user data
+        session([
+            'user' => [
+                'id'       => $user->id,
+                'username' => $user->username
+            ]
+        ]);
+
+        echo 'Login efetuado com sucesso';
+
+        return null;
     }
 
     public function logout(): void
